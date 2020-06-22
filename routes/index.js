98845d461ea7c6e8178c6286dd615a7a1e8559f9
Joe Thompson <jthompso@mtu.edu>
2020-06-21 21:56:56 -0700

    Added new message form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi There!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'Create Message'})
+})
+
+router.post('/new', function(req, res, next) {
+  let messageUser = req.body.messageUser
+  let messageText = req.body.messageText
+  messages.push({text: messageText, user: messageUser, added: new Date()})
+  res.redirect('/')
+})
 
 module.exports = router;
